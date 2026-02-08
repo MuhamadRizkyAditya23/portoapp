18cf3de33360c32d98384a1a6e28f71c170eb83d
anggablangkon <anggakurniawan135@gmail.com>
2026-02-08 09:12:22 +0700

    naik

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,14 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/tentangkami', function () {
+    return view('admin.dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
